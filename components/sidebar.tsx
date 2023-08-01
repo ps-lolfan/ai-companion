443b61345741e33b536eed0f1143597724835ed3
ps-lolfan <pbsinnur@gmail.com>
2023-08-01 13:17:17 +0530

    sidebar done for mobile

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -3,11 +3,12 @@
 import { cn } from "@/lib/utils";
 import { Home, Plus, Settings } from "lucide-react";
 
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 export const Sidebar = () => {
 
     const pathname = usePathname();
+    const router = useRouter();
 
     const routes = [
         {
@@ -29,12 +30,20 @@ export const Sidebar = () => {
             pro: true,
         }
     ]
+
+    const onNavigate = (url: string, pro: boolean) => {
+        // TODO: check if pro
+
+        return router.push(url);
+    }
+
     return (
         <div className="space-y-4 flex flex-col h-full text-primary bg-secondary">
             <div className="p-3 flex flex-1 justify-center">
                 <div className="space-y-2">
                     {routes.map((route) => (
                         <div
+                            onClick={() => onNavigate(route.href, route.pro)}
                             key={route.href}
                             className={cn(
                                 "text-muted-foreground text-xs group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-primary hover:bg-primary/10 rounded-lg transtion",
